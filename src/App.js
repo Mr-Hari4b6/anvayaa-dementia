79b9c89340f57c9d362036df275b86df603ebd3e
Ramya-6281 <141622068+Ramya-6281@users.noreply.github.com>
2024-01-24 15:34:08 +0530

    new screen profile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
+// import './App.css';
+// import { AuthenticationForm } from './pages/Authentication/index.tsx';
+// import Dashboard from './pages/Layout/index.tsx';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Dashboard></Dashboard>
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import './App.css';
-import { AuthenticationForm } from './pages/Authentication/index.tsx';
+import Dashboard from './pages/Layout/index.tsx';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Profile from './pages/Profile/profile.tsx';
 
-function App() {
-  return (
-    <div className="App">
-      <AuthenticationForm></AuthenticationForm>
-    </div>
-  );
+
+
+
+function AppRouting() {
+ 
+ const navigation = useNavigate()
+ 
+  
+    return (
+        <>
+        
+              
+
+                <Dashboard>
+                        <Routes>
+                            <Route path="/profile" element={<Profile/>}/>
+                        </Routes>
+                        </Dashboard >
+                       
+                   
+            
+           
+ 
+        </>
+    );
 }
-
-export default App;
+ 
+export default AppRouting;
+ 
--- src/pages/Profile/profile.tsx
+++ src/pages/Profile/profile.tsx
@@ -1,55 +1,24 @@
 
-import React, {ReactNode} from 'react';
-
-import type { MenuProps } from 'antd';
+import React from 'react';
 import { Layout, Menu, theme, Radio, Select } from 'antd';
+import { Input } from 'antd';
+import{Form} from 'antd';
+import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
+import { NavLink } from 'react-router-dom';
 
 import { DatePicker, Space } from 'antd';
 
 const { Header, Content, Sider } = Layout;
-// const { TextArea } = Input;
+const { TextArea } = Input;
+
+dayjs.extend(customParseFormat);
 
 const { RangePicker } = DatePicker;
 const dateFormatList = ['DD/MM/YYYY', 'DD/MM/YY', 'DD-MM-YYYY', 'DD-MM-YY'];
 
-const items2: MenuProps['items'] = [
-  {
-    key: 'profile',
-    label: 'Profile',
-    path:"/profile",
-    
-  },
-  {
-    key: 'password',
-    label: 'Password',
-  },
-  {
-    key: 'assessments',
-    label: 'Assessments',   
-  },
-  {
-    key: 'Activities',
-    label: 'Activities',
-  },
-  {
-    key: 'Adding Remainders',
-    label: 'Adding Remainders',
-  },
-  {
-    key: 'View Details',
-    label: 'View Details',
-  },
-  {
-    key: 'Completed Activites',
-    label: 'Completed Activites',
-  },
-];
-interface SidebarProps {
-    children: ReactNode;
-}
- 
-const DashBoard: React.FC<SidebarProps> = ({children}) => {
+
+const Profile: React.FC = () => {
   const { token } = theme.useToken();
 
   const handleMenuItemClick = (key: string | number) => {
@@ -65,20 +34,8 @@ const DashBoard: React.FC<SidebarProps> = ({children}) => {
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} style={{ flex: 1, minWidth: 0 }} />
       </Header>
       <Content style={{ padding: '0 48px' }}>
-        <Layout style={{ padding: '24px 0', background: token.colorBgContainer, borderRadius: token.borderRadiusLG }}>
-          <Sider style={{ background: token.colorBgContainer }} width={200}>
-            <Menu mode="inline"
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
-              style={{ height: '100%' }}
-              items={items2.map((item:any) => ({
-                ...item,
-                onClick: () => handleMenuItemClick(item.key),
-              }))}
-            />
-          </Sider>
-            
- {/* <div id="container" style={{ padding: '10px', color: 'purple' }}>
+        <Layout style={{ padding: '24px 0', background: token.colorBgContainer, borderRadius: token.borderRadiusLG }}>       
+ <div id="container" style={{ padding: '10px', color: 'purple' }}>
   <Form labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
     <Form.Item  style={{ marginBottom: '5px', color: 'purple' }}>
     <span  style={{marginRight:'10rem'}}>PWD Name</span>
@@ -119,33 +76,35 @@ const DashBoard: React.FC<SidebarProps> = ({children}) => {
 </div>  
 <div>
 <Form.Item  style={{ marginBottom: '5px', color: 'purple' }}>
-    <span style={{marginRight:'10rem'}}>Occupation</span>
-      <Input placeholder="occupation" />
+    <span style={{marginRight:'10rem'}}>Responder Name</span>
+      <Input placeholder="respondername" />
     </Form.Item>
     <Form.Item  style={{ marginBottom: '5px', color: 'purple' }}>
-    <span style={{marginRight:'10rem'}}>Occupation</span>
-      <Input placeholder="occupation" />
+    <span style={{marginRight:'10rem'}}>Responder Relation</span>
+      <Input placeholder="responderrelation" />
     </Form.Item>
     <Form.Item  style={{ marginBottom: '5px', color: 'purple' }}>
-    <span style={{marginRight:'10rem'}}>Occupation</span>
-      <Input placeholder="occupation" />
+    <span style={{marginRight:'10rem'}}>Primary Care  Giver  Name</span>
+      <Input placeholder="primarycaregivername" />
     </Form.Item>
     <Form.Item  style={{ marginBottom: '5px', color: 'purple' }}>
-    <span style={{marginRight:'10rem'}}>Occupation</span>
-      <Input placeholder="occupation" />
+    <span style={{marginRight:'10rem'}}>Current Home Care Attender</span>
+      <Input placeholder="currenthomecareattender" />
     </Form.Item>
-    <Form.Item label="Area">
-          <Select>
-            <Select.Option value="area">Area</Select.Option>
-          </Select>
-        </Form.Item>
-</div> */}
-
+    <Form.Item label="Selectlanguages" name="area">
+  <Select>
+    <Select.Option value="Telugu">Telugu</Select.Option>
+    <Select.Option value="Hindi">Hindi</Select.Option>
+    <Select.Option value="English">English</Select.Option>
+  </Select>
+</Form.Item>
+</div>
         </Layout>
-        <main>{children}</main>
       </Content>
     </Layout>
   );
 };
 
-export default DashBoard;
+export default Profile;
+
+
